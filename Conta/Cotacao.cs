34e58200a4b8517c49a55c1139d31144b318ff8d
franciscoamartin <franciscoaugustomartin@gmail.com>
2019-09-07 15:33:43 -0300

    Projeto c# 2

---
+++ Conta/Cotacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Conta {
+    class Cotacao {
+
+        public double valor;
+        public double dolar;
+        public double iof = 6;
+
+
+
+        public double Conversao(double valor, double dolar) {
+            return (valor * dolar);
+        }
+
+        public double Iof(double dolar) {
+            return (iof * dolar / 100.00);
+        }
+
+
+    }
+}
---
+++ Conta/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conta {
+    class Program {
+        static void Main(string[] args) {
+
+            Cotacao func = new Cotacao();
+
+            Console.Write("Qual é a cotação do dolar?  ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double cotacaoFinal = func.Conversao(cotacao, compra);
+            double iofFinal = func.Iof(cotacaoFinal) + cotacaoFinal;
+
+            Console.Write("Valor a ser pago em reais = " + iofFinal, "F2", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
+
